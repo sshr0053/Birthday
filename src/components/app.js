47775b22c3955d7fb77560cb015ac5c2407498b8
sshr0053 <sshr0053@yahoo.com>
2018-07-14 11:03:14 -0600

    style fixes

--- src/components/app.js
+++ src/components/app.js
@@ -38,7 +38,6 @@ Button('Generate Countdown', () => this.setState({ active: true}))
       <div className="grid">
       <h1 className="grid__title">Birthday Countdown</h1>
 
-      <div className="grid__skew-dark-one-box"></div>
       <div className="grid__skew-dark-two"></div>
       <div className="grid__skew-dark-three"></div>
    
